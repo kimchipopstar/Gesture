0a74a070db961aa8328f5d06fa9834fa76f9add8
J.K <K_940201@hotmail.com>
2017-08-03 20:27:00 -0700

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by Jaewon Kim on 2017-08-03.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gestures
+//
+//  Created by Jaewon Kim on 2017-08-03.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
